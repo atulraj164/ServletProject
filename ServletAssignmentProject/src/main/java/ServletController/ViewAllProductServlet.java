faf30ba05e431c63cc8165dcaa1ccf536afa027e
atulraj164 <136972296+atulraj164@users.noreply.github.com>
2026-07-13 22:01:11 +0530

    Refactor ViewAllProductServlet to use JSP for output
    Refactored doGet method to forward product list to viewStore.jsp instead of printing HTML.

--- ServletAssignmentProject/src/main/java/ServletController/ViewAllProductServlet.java
+++ ServletAssignmentProject/src/main/java/ServletController/ViewAllProductServlet.java
@@ -18,29 +18,10 @@ public class ViewAllProductServlet extends HttpServlet{
 		 StoreDao dao = new StoreDao();
 
 	        List<Store> productList = dao.getAllStoreProducts();
-
-	        res.setContentType("text/html");
-
-	        PrintWriter out = res.getWriter();
-
-	        out.println("<html>");
-	        out.println("<body>");
-
-	        out.println("<h2>Store Products</h2>");
-
-	        for (Store store : productList) {
-
-	            out.println("<p>");
-	            out.println("ID : " + store.getId());
-	            out.println("<br>");
-	            out.println("Name : " + store.getName());
-	            out.println("</p>");
-	            out.println("<hr>");
-
-	        }
-
-	        out.println("</body>");
-	        out.println("</html>");
+	        req.setAttribute("allProduct", productList);
+            RequestDispatcher sd=req.getRequestDispatcher("viewStore.jsp");
+            sd.forward(req, res);
+	    
 	    }
 		
 }

ec2898178eaf708fb2a485199732b55043ca7e9f
atulraj164 <136972296+atulraj164@users.noreply.github.com>
2026-07-18 21:33:27 +0530

    Refactor CustomerDao to handle additional customer fields
    Updated CustomerDao to include email, password, and mobile fields in customer operations. Added method to check customer credentials.

--- ServletAssignmentProject/src/main/java/Dao/CustomerDao.java
+++ ServletAssignmentProject/src/main/java/Dao/CustomerDao.java
@@ -12,9 +12,13 @@ package Dao;
 
 	public class CustomerDao {
 
-	    String INSERTQUERY = "INSERT INTO customer(id, name, address) VALUES(?, ?, ?)";
-	    String GETALLCUSTOMERS = "SELECT * FROM customer";
-
+		private static final String INSERTQUERY =
+			    "INSERT INTO customer(name, email, password, mobile, address) VALUES(?, ?, ?, ?, ?)";
+		private static final String GETALLCUSTOMERS =
+			    "SELECT * FROM customer";
+		
+		private static final String GETCUSTOMERBYEMAIL = "SELECT * FROM customer WHERE email = ?";
+		
 	    Connection Sc = null;
 	    PreparedStatement ps = null;
 
@@ -28,9 +32,11 @@ package Dao;
 
 	            ps = Sc.prepareStatement(INSERTQUERY);
 
-	            ps.setInt(1, customer.getId());
-	            ps.setString(2, customer.getName());
-	            ps.setString(3, customer.getAddress());
+	            ps.setString(1, customer.getName());
+	            ps.setString(2, customer.getEmail());
+	            ps.setString(3, customer.getPassword());
+	            ps.setString(4, customer.getMobile());
+	            ps.setString(5, customer.getAddress());
 
 	            int success = ps.executeUpdate();
 
@@ -44,7 +50,6 @@ package Dao;
 	            e.printStackTrace();
 	        }
 	    }
-
 	    // Fetch All Customers
 	    public List<Customer> getAllCustomers() {
 
@@ -65,6 +70,9 @@ package Dao;
 
 	                customer.setId(rs.getInt("id"));
 	                customer.setName(rs.getString("name"));
+	                customer.setEmail(rs.getString("email"));
+	                customer.setPassword(rs.getString("password"));
+	                customer.setMobile(rs.getString("mobile"));
 	                customer.setAddress(rs.getString("address"));
 
 	                customerList.add(customer);
@@ -77,4 +85,27 @@ package Dao;
 	            return null;
 	        }
 	    }
+	    public boolean checkCustomer(String email, String password) {
+
+	        try {
+	            storeConnection storeconnection = new storeConnection();
+	            Sc = storeconnection.getStoreConnection();
+
+	            ps = Sc.prepareStatement(GETCUSTOMERBYEMAIL);
+	            ps.setString(1, email);
+
+	            ResultSet rs = ps.executeQuery();
+
+	            if (rs.next()) {
+	                return password.equals(rs.getString("password"));
+	            }
+
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+
+	        return false;
+	    }
+	    
+	    
 	}
